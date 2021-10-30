2145a91e67121b13c1e0b3fdefe6ddb0252e4d27
Carles Rafols <41967813+karb94@users.noreply.github.com>
2021-10-30 19:43:14 +0100

    First commit

---
+++ nvim/dot-config/nvim/after/ftplugin/tex.vim
@@ -0,0 +1,3 @@
+" Set textwidth to automatically break line if longer than 80
+set textwidth=79
+set formatoptions=rqjt
---
+++ nvim/dot-config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,40 @@
+local nvim_lsp = require("lspconfig")
+
+local M = {}
+
+M.on_attach = function(client, bufnr)
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	local lsp_nmap = function(key, lsp_func)
+		local func = "<cmd>lua vim.lsp." .. lsp_func .. "<CR>"
+		local opts = { noremap = true, silent = true }
+		vim.api.nvim_buf_set_keymap(bufnr, "n", key, func, opts)
+	end
+
+	-- Mappings.
+	lsp_nmap("gD", "buf.declaration()")
+	lsp_nmap("gd", "buf.definition()")
+	lsp_nmap("<C-k>", "buf.signature_help()")
+	lsp_nmap("<leader>e", "diagnostic.show_line_diagnostics()")
+	lsp_nmap("[d", "diagnostic.goto_prev()")
+	lsp_nmap("]d", "diagnostic.goto_next()")
+end
+
+local servers = {
+	"pyright",
+	"clangd",
+	"bashls",
+	"texlab",
+	"vimls",
+}
+
+for _, lsp in ipairs(servers) do
+	nvim_lsp[lsp].setup({
+		on_attach = M.on_attach,
+		flags = {
+			debounce_text_changes = 150,
+		},
+	})
+end
+
+return M
