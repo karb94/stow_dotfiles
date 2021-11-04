62581393b54bb13e34fdcc8dddfb6649619f63d9
Carles Rafols <41967813+karb94@users.noreply.github.com>
2021-11-04 19:42:54 +0000

    Fast push

--- nvim/dot-config/nvim/after/ftplugin/tex.vim
+++
@@ -1,3 +0,0 @@
-" Set textwidth to automatically break line if longer than 80
-set textwidth=79
-set formatoptions=rqjt
--- nvim/dot-config/nvim/lua/plugins/kommentary.lua
+++
@@ -1,4 +0,0 @@
-require('kommentary.config').configure_language("default", {
-    prefer_single_line_comments = true,
-    prefer_multi_line_comments = false,
-})
